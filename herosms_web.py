526ee74eae7aa7c3738d95f91e095339dc34e485
iqbaalfrm <iqbaalfirmansyah2211@gmail.com>
2026-03-08 11:25:05 +0700

    Switch country back to Colombia

--- herosms_web.py
+++ herosms_web.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 API_KEY = "3f392525d7d57d7fB04f819627d29202"
 BASE_URL = "https://hero-sms.com/stubs/handler_api.php"
 SERVICE = "wa"
-COUNTRY = "4"
+COUNTRY = "33"
 MAX_PRICE = "2.2"
 DEFAULT_TIMEOUT = 600
 POLL_INTERVAL = 5
